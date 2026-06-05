740c920072e9b6673be970c133bd5f058f9757a9
milserbua <ldelmarco@office.htlinn.ac.at>
2026-06-05 19:24:33 +0200

    Add progressive difficulty scaling to platform spawning

--- meista/platformspawner.gd
+++ meista/platformspawner.gd
@@ -5,32 +5,42 @@ extends Node3D
 
 var spawn_z = 0.0
 var spawn_distance = 10.0
+var difficulty_multiplier = 1.0
+var platforms_spawned = 0
 
 func _ready():
 	for i in range(20):
 		spawn_platform()
 
 func _process(delta):
-
-	# nur nach vorne spawnen, nicht bei Stillstand spammen
+	# Spawn platforms ahead of player with difficulty scaling
 	if player.position.z < spawn_z + 40:
 		spawn_platform()
+	
+	# Progressive difficulty increase
+	difficulty_multiplier = 1.0 + (platforms_spawned / 50.0) * 0.3
 
 func spawn_platform():
 	var platform = platform_scene.instantiate()
-
-	# FIX: nur Z basiert, kein Chaos
+	
+	# Randomize X position based on difficulty
+	var x_variation = randf_range(-1.5, 1.5) * difficulty_multiplier
+	
 	platform.position = Vector3(
-		0.0, #X
-		randf_range(-1.0, 1.0),  # Y Variation
+		x_variation,
+		randf_range(-0.8, 1.2),
 		spawn_z
 	)
+	
 	add_child(platform)
 	spawn_z -= spawn_distance
+	platforms_spawned += 1
 	
 func reset_world():
 	for child in get_children():
 		child.queue_free()
-		spawn_z = 0.0
+	spawn_z = 0.0
+	platforms_spawned = 0
+	difficulty_multiplier = 1.0
 	for i in range(20):
 		spawn_platform()
