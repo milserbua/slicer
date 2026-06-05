2250e665165432d525180db7761e4518b7bb37e6
milserbua <ldelmarco@office.htlinn.ac.at>
2026-06-05 19:25:16 +0200

    Add optional platform oscillation mechanic for dynamic gameplay

--- meista/platform.gd
+++ meista/platform.gd
@@ -2,11 +2,17 @@ extends Node3D
 
 @export var move_speed = 2.0
 @export var move_range = 2.0
+@export var oscillate = false
 
 var start_pos
+var time_offset = 0.0
 
 func _ready():
 	start_pos = position
+	time_offset = randf_range(0.0, 2.0 * PI)
 
-#func _process(delta):
-	#position.z = start_pos.z + sin(Time.get_time_dict_from_system()["second"] + position.x) * move_range
+func _process(delta):
+	if oscillate:
+		# Subtle platform movement for added challenge
+		position.x = start_pos.x + sin(Time.get_ticks_msec() / 1000.0 + time_offset) * move_range
+		position.y = start_pos.y + cos(Time.get_ticks_msec() / 1500.0 + time_offset) * (move_range * 0.5)
